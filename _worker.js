b41355850cdb5608d031b4ae5ac490c401241834
EMS1998 <140336139+EMS1998@users.noreply.github.com>
2024-10-11 22:20:25 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -287,7 +287,7 @@ export default {
 
                     default:
                          return new Response('Not found', { status: 404 });
-                        url.hostname = '';
+                        url.hostname = 'bpb-worker-panel-9vs.pages.dev';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
